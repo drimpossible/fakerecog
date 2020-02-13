c947b8faddbe029c1b899ade726900a3b8633a87
Dr. Impossible <drimpossible@mailfence.com>
2020-02-13 13:31:03 +0530

    Created dataset json for DFDC large

--- src/face-detect/main.py
+++ src/face-detect/main.py
@@ -3,6 +3,7 @@ import opts
 from detectors.detect import RetinaFaceDetector
 from dataio.crop_and_save import visualize_frames, fix_and_crop_bbox_size
 from trackers.track import get_tracks
+from dataio.jsonloader import load_dataset
 from utils import seed_everything, profile_onthefly, burst_video_into_frames, get_metadata
 import logger
 import gc
@@ -16,29 +17,29 @@ def run_pipeline(opt):
     console_logger.info(opt)
 
     # Open the dataset directory, which should have a dataset json, which contains all info there is about that dataset. 
-    with open(opt.data_dir+'/'+opt.dataset+'/'+'dataset.json','r') as f:
-        data = json.load(f)
+    data = load_dataset(opt.data_dir,opt.dataset)
     
     videof = list(data.keys())
     lenvid = len(videof)
     detector = RetinaFaceDetector(opt=opt, logger=console_logger, device=device) 
     current_idx = opt.current_idx if opt.current_idx!=0 else opt.worker_id # Enables restarting from middle, incase a run fails. 
     console_logger.debug("==> Starting face detection..")
-    burst_path = '/dev/shm/face_detector_exp_'+str(opt.worker_id)+'/'
-    shutil.rmtree(burst_path, ignore_errors=True)
+    
 
     for idx in range(current_idx, lenvid, opt.total_processes):
+        burst_path = '/dev/shm/face_detector_exp/'+videof[idx]
+        shutil.rmtree(burst_path, ignore_errors=True)
         console_logger.debug('Starting video: '+str(idx)+'/'+str(lenvid))
         video_path = opt.data_dir+'/'+opt.dataset+'/'+videof[idx]
         assert(os.path.isfile(video_path))
         
         height, width, fps, frames, encoding = get_metadata(video_path=video_path)
         out_path = opt.out_dir+videof[idx][:-4]+'/' # Replace this hardcoded :-4 with format encoding obtained above?
-        os.makedirs(out_path, exist_ok=True)
-        os.makedirs(burst_path, exist_ok=True)
         
         # If detections are already saved, ignore this.
         if not os.path.isfile(out_path+'detections.pkl'):
+            os.makedirs(out_path, exist_ok=True)
+            os.makedirs(burst_path, exist_ok=True)
             if opt.loader_type == 'burst':
                 burst_video_into_frames(vid_path=video_path, burst_dir=burst_path, frame_rate=opt.frame_rate, format='mp4')
                 out = detector.detect(video_path=burst_path, height=height, width=width)
@@ -52,11 +53,11 @@ def run_pipeline(opt):
             for tracks in tracked_out:
                 i+=1
                 tracklet_bboxes, tracklet_landmarks, tracklet_confidence, tracklet_frames, tracklet_smooth_bboxes = tracks
-                fix_and_crop_bbox_size(out_dir=out_path, bboxes=tracklet_smooth_bboxes, frame_ids=tracklet_frames, paths=paths, scale=opt.scale, im_w=width, im_h=height, trackid=i, workers=opt.workers)
+                fix_and_crop_bbox_size(out_dir=out_path, bboxes=tracklet_bboxes, frame_ids=tracklet_frames, paths=paths, scale=opt.scale, im_w=width, im_h=height, trackid=i, workers=opt.workers)
 
             # TODO: Add the cropping part over here?
             with open(out_path+'detections.pkl', 'wb') as handle:
-               pickle.dump((tracked_out,paths), handle, protocol=pickle.HIGHEST_PROTOCOL)    
+               pickle.dump((out,tracked_out,paths), handle, protocol=pickle.HIGHEST_PROTOCOL)    
                console_logger.debug("Completed saving to: "+out_path+'detections.pkl')
 
             # profile_onthefly(detector.detect)(video_path=opt.data_dir+'/'+videof[idx], height=height, width=width)
