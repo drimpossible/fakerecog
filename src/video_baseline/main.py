33912832a5c399c74ea641d7d56160c7a9b76033
joanna <joanna.materzynska@gmail.com>
2020-03-19 14:50:22 +0000

    add correct weights

--- src/video_baseline/main.py
+++ src/video_baseline/main.py
@@ -151,7 +151,7 @@ def main_worker(gpu, ngpus_per_node, args):
     else:
         model = torch.nn.DataParallel(model).cuda()
     # define loss function (criterion) and optimizer
-    weights = [0.2, 0.8]
+    weights = [0.8, 0.2]
     class_weights = torch.FloatTensor(weights).cuda(args.gpu)
     criterion = nn.CrossEntropyLoss(weight=class_weights).cuda(args.gpu)
 
